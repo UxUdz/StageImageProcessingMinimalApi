01b682c40ab9803cecd12bd4c2705738fb44c96c
UxUdz <fouadjeloule09@gmail.com>
2024-02-13 14:49:00 +0100

    add missing filles

--- StageImageProcessingMinimalApi/obj/Release/net7.0/PubTmp/Out/wwwroot/Decrypt/DecryptScript.js
+++
@@ -1,5 +0,0 @@
-function showContainer() {
-var rightContainer = document.querySelector('.right-container');
-
-rightContainer.style.display = (rightContainer.style.display === 'none' || rightContainer.style.display === '') ? 'flex' : 'none';
-}
